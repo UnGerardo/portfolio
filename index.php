4017c94577951998b7b174f67aeec2c97abc387a
UnGerardo <gerardojdls@gmail.com>
2024-09-26 20:27:03 -0700

    - Initialized git - Created index.php - Added basic styles

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="styles.css">
+  <title>Gerardo's Portfolio</title>
+</head>
+<body>
+  <h1>Gerardo's Portfolio</h1>
+  <main>
+    <section>
+      <a><h3>Project 1</h3></a>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptas provident delectus iusto minus harum illum ab tempore porro. Maiores tenetur, nobis corporis vero alias quae enim ratione earum sit commodi hic eaque necessitatibus magnam recusandae suscipit non a. Minima nisi quaerat, rerum laudantium molestias debitis sed aliquam laborum rem!</p>
+    </section>
+    <section>
+      <a><h3>Project 2</h3></a>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptas provident delectus iusto minus harum illum ab tempore porro. Maiores tenetur, nobis corporis vero alias quae enim ratione earum sit commodi hic eaque necessitatibus magnam recusandae suscipit non a. Minima nisi quaerat, rerum laudantium molestias debitis sed aliquam laborum rem!</p>
+    </section>
+    <section>
+      <a><h3>Project 3</h3></a>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet voluptas provident delectus iusto minus harum illum ab tempore porro. Maiores tenetur, nobis corporis vero alias quae enim ratione earum sit commodi hic eaque necessitatibus magnam recusandae suscipit non a. Minima nisi quaerat, rerum laudantium molestias debitis sed aliquam laborum rem!</p>
+    </section>
+  </main>
+  <footer>
+    <p>github</p>
+    <p>linkedin</p>
+  </footer>
+</body>
+</html>
