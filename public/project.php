ea6f67fee25c8f903da86b5765587a5f5b0ce822
UnGerardo <gerardojdls@gmail.com>
2024-09-29 20:26:12 -0700

    - Created a basic file structure

---
+++ public/project.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types = 1); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><?php echo 'Project Name' ?></title>
+</head>
+<body>
+  
+</body>
+</html>
