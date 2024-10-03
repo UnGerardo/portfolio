4647f7e8cb5b0c2a98f446d734040bf963de5f50
UnGerardo <gerardojdls@gmail.com>
2024-10-02 22:34:41 -0700

    - Query project from URL - Load project info

--- public/project.php
+++ public/project.php
@@ -1,12 +1,27 @@
-<?php declare(strict_types = 1); ?>
+<?php
+  declare(strict_types = 1);
+  require 'connect.php';
+
+  $projectName = $_GET['project'];
+  $query = 'SELECT * FROM projects WHERE projectName=\'' . $projectName . '\'';
+  $projectQuery = $connection->query($query);
+  $projectInfo = mysqli_fetch_array($projectQuery);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/styles.css">
   <title><?php echo 'Project Name' ?></title>
 </head>
 <body>
-
+  <section>
+    <a href="<?php echo $projectInfo['projectLink'] ?>">
+      <h2><?php echo $projectInfo['projectName'] ?></h2>
+    </a>
+    <p><?php echo $projectInfo['projectDescription'] ?></p>
+  </section>
+  <?php include 'footer.php' ?>
 </body>
 </html>
