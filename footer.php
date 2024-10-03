2422da5b73400d6ba2dc59b98731e922c9fbee35
UnGerardo <gerardojdls@gmail.com>
2024-10-02 22:07:35 -0700

    - Added external links - Improved external links styles

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer>
-  <p>github</p>
-  <p>linkedin</p>
+  <a href="https://github.com/UnGerardo/"  target="_blank" class="external_link" >GitHub</a>
+  <a href="https://www.linkedin.com/in/gerardojls/" target="_blank" class="external_link" >LinkedIn</a>
 </footer>
